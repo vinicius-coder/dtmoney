3655a214733218c580efd6b5bb18de91364a867c
Avonts <vinialves022@gmail.com>
2021-03-19 20:18:52 -0300

    Add axios | mirageJs | react-modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,13 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
     
     :root {
-        --background: '#f0f2f5';
+        --background: #f0f2f5;
         --red: #E52E4D;
         --green: #33CC95;
         --blue: #5429CC;
         --blue-light: #6933FF;
         --text-title: #363F5F;
-        --text-body: 969CB3;
+        --text-body: #969CB3;
         --background: #F0F2F5;
         --shape: #FFFFFF;
     }
