097550accc10665dcf8470b35d8dde0d19773b0e
Avonts <vinialves022@gmail.com>
2021-03-22 15:46:46 -0300

    Add TransactionContext

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import { Container } from "./styles";
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from "../../TransactionsContext";
 
 
 export function Summary(){
+
+    const transactions = useContext(TransactionsContext);
+    console.log(transactions);
+
     return (
        <Container>
            <div>
