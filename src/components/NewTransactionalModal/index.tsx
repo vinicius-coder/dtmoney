a8d2c4eb326a8054ac74696e631ed934d80a43f6
Avonts <vinialves022@gmail.com>
2021-03-22 16:44:51 -0300

    Add Transaction post

--- src/components/NewTransactionalModal/index.tsx
+++ src/components/NewTransactionalModal/index.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import Modal from 'react-modal';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
 
 interface NewTransactionalModalProps {
     isOpen: boolean;
@@ -14,21 +14,29 @@ interface NewTransactionalModalProps {
 
 export function NewTransactionalModal({ isOpen, onRequestClose }: NewTransactionalModalProps) {
 
+    const { createTransaction } = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-        const data = {
-            title,
-            value,
-            category,
-            type
-        }
 
-        api.post('transactions', data)
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type,
+        })
+
+
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
@@ -57,8 +65,8 @@ export function NewTransactionalModal({ isOpen, onRequestClose }: NewTransaction
                 <input
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,34 +7,34 @@ import totalImg from '../../assets/total.svg';
 import { TransactionsContext } from "../../TransactionsContext";
 
 
-export function Summary(){
+export function Summary() {
 
-    const transactions = useContext(TransactionsContext);
+    const { transactions } = useContext(TransactionsContext);
     console.log(transactions);
 
     return (
-       <Container>
-           <div>
-               <header>
-                   <p>Entradas</p>
-                   <img src={incomeImg} alt="Entradas"/>
-               </header>
-               <strong>R$1000,00</strong>
-           </div>
-           <div>
-               <header>
-                   <p>Saídas</p>
-                   <img src={outcomeImg} alt="Saídas"/>
-               </header>
-               <strong>- R$500,00</strong>
-           </div>
-           <div className="highlight-background">
-               <header>
-                   <p>Total</p>
-                   <img src={totalImg} alt="Total"/>
-               </header>
-               <strong>R$500,00</strong>
-           </div>
-       </Container>
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>- R$500,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
     );
 }
